93aac60d4eec9d0d885c60c1cf539fa3d407a272
PauCastano <pau.castano@estudiantat.upc.edu>
2022-04-20 16:47:54 +0200

    NOT, AND, OR

--- Puertas.py
+++ Puertas.py
@@ -1,2 +1,30 @@
+class PuertaLogica:
 
-def AND(self, entrada1, entrada2, salida)
+    def __init__(self, entrada1 = 0, entrada2 = 0):
+
+        self.entrada1 = entrada1
+        self.entrada2 = entrada2
+
+    def NOT(self):
+        if self.entrada1 == 1:
+            salida = 0
+        else:
+            salida = 1
+        return salida
+
+    def AND(self):
+        if self.entrada1 == 1 and self.entrada2 == 1:
+            salida = 1
+        else:
+            salida = 0
+        return salida
+
+    def OR(self):
+        if self.entrada1 == 1 and self.entrada2 == 1:
+            salida = 1
+        elif self.entrada1 == 1 and self.entrada2 == 1:
+            salida = 1
+        elif self.entrada1 == 1 and self.entrada2 == 1:
+            salida = 1
+        else:
+            salida =
