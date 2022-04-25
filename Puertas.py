cd303549eebf0d0458836b1496a411f60b635bd2
PauCastano <pau.castano@estudiantat.upc.edu>
2022-04-25 16:17:08 +0200

    Todas las puertas (AND; OR; XOR; NAND; NOR; XNOR)

--- Puertas.py
+++ Puertas.py
@@ -5,13 +5,6 @@ class PuertaLogica:
         self.entrada1 = entrada1
         self.entrada2 = entrada2
 
-    def NOT(self):
-        if self.entrada1 == 1:
-            salida = 0
-        else:
-            salida = 1
-        return salida
-
     def AND(self):
         if self.entrada1 == 1 and self.entrada2 == 1:
             salida = 1
@@ -20,11 +13,38 @@ class PuertaLogica:
         return salida
 
     def OR(self):
-        if self.entrada1 == 1 and self.entrada2 == 1:
+        if self.entrada1 == 1:
             salida = 1
-        elif self.entrada1 == 1 and self.entrada2 == 1:
-            salida = 1
-        elif self.entrada1 == 1 and self.entrada2 == 1:
+        elif self.entrada2 == 1:
             salida = 1
         else:
-            salida =
+            salida = 0
+        return salida
+
+    def XOR(self):
+        if self.entrada1 != self.entrada2:
+            salida = 1
+        else:
+            salida = 0
+        return salida
+
+    def NAND(self):
+        if self.entrada1 == 1 and self.entrada2 == 1:
+            salida = 0
+        else:
+            salida = 1
+        return salida
+
+    def NOR(self):
+        if (self.entrada1 == 0) and (self.entrada2 == 0):
+            salida = 1
+        else:
+            salida = 0
+        return salida
+
+    def XNOR(self):
+        if self.entrada1 == self.entrada2:
+            salida = 1
+        else:
+            salida = 0
+        return salida
