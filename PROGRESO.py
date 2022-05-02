6fc1b372ac21d7bf706b57bbcabdd73379dd6d7b
guillemrh <guillemmm.98@gmail.com>
2022-05-02 15:41:45 +0200

    Classe progresso

--- PROGRESO.py
+++ PROGRESO.py
@@ -1,9 +1,8 @@
 from NIVEL import *
 class PROGRESO:
     # Guardamos el progreso de los niveles
-    progreso = 0
+
     def comp(self):
         # Comportamineto de la Puerta logica
-        if snivel == 1:
-            progreso = progreso +1
+        progreso = nivel_actual - 1
         return progreso
