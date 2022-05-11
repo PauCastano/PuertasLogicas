219a6cf06e4c4e64beee3aa7bab17a2dba60ac39
guillemrh <guillemmm.98@gmail.com>
2022-05-11 15:27:06 +0200

    codi per moure objectes

--- PuertasL.py
+++ PuertasL.py
@@ -14,8 +14,6 @@
 # ***************************
 from Pieza import *
 
-https://www.youtube.com/watch?v=ZFI4vExV2F0
-
 import pygame
 from pygame.locals import *
 import os
@@ -74,11 +72,43 @@ def main():
     # se muestran lo cambios en pantalla
     pygame.display.flip()
 
+    ventana = pygame.display.set_mode((600,300))
+    pygame.display.set_caption("Tutorial")
+    posX=200
+    posY = 100
+    velocidad = 2
+    Blanco(255, 255, 255)
+    derecha = True
+    rectangulo = pygame.Rect(0,0,100,150)
+    rectangulo_dos= pygame.Rect(300, 300, 100, 150)
     # el bucle principal del juego
     while True:
+
+        ventana.fill(Blanco)
+        pygame.draw.rect(ventana, (180, 70, 70), rectangulo_dos)
+        pygame.draw.rect(ventana, (180, 70, 70), rectangulo)
+        rectangulo.left, rectangulo.top = pygame.mouse.get_pos()
+
+        if rectangulo.colliderect(rectangulo_dos):
+            velocidad = 0
+            print("Colisiono")
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        if derecha == True:
+            if posX < 400:
+                posX += velocidad
+                rectangulo_dos.left = posX
+            else:
+                derecha = False
+        else:
+            if posX > 1:
+                posX -= velocidad
+                rectangulo_dos.left = posX
+            else:
+                derecha = True
 
 if __name__ == "__main__":
     main()
