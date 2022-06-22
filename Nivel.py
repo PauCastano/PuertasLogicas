88e7c91f97fa58a6a4ff72d28a8e641839ea6b8b
guillemrh <guillemmm.98@gmail.com>
2022-06-22 11:12:33 +0200

    22/06 pieza, and, comp de hueco, pieza y nivel

--- Nivel.py
+++ Nivel.py
@@ -9,20 +9,23 @@ from AND import *
 class Nivel:
     pygame.init()
 
-    def __init__(self, n_input, pieza = []):
+    def __init__(self, n_input, piezas = []):
 
         self.caracteristcas = [random.randint(0, 1) for i in
                                range(n_input + 1)]  # devuelve una lista de 5 valores entre 0 y 1,
         self.input = self.caracteristcas[:2]  # en esta lista estan los inputs y los outputs
         self.output = self.caracteristcas[-1]
         self.n_hueco = n_input - 1
-        self.pieza = [AND()]  # listado de piezas utilizado en el nivel
+        self.piezas = [AND()]  # listado de piezas utilizado en el nivel
         self.posible_solucion = []
 
     def rellenar_huecos(self):
+#Poner piezas aleatorias para cada hueco
+        if self.piezas[0].comp(self.input) == self.output:
+            self.posible_solucion.append(self.piezas[0])
 
-        if self.pieza[0].comp(self.input) == self.output:
-            self.posible_solucion.append(self.pieza[0])
+    def comp(self, inputs):
+        pass
 
 
 if __name__ == '__main__':
@@ -30,7 +33,7 @@ if __name__ == '__main__':
 
     print('Entradas:', n.input)
     print('Salida querida:', n.output)
-    print('Resultado:', n.pieza[0].comp(n.input))
+    print('Resultado:', n.piezas[0].comp(n.input))
     print(n.posible_solucion)
 
 
--- Pieza.py
+++ Pieza.py
@@ -3,15 +3,13 @@ import random
 
 class Pieza:
     # Definicion del comportamiento de las piezas en general
-    def __init__(self,tamanyo, cordenadas=None, entradas=None):
-
-        if entradas is None:
-            entradas = [random.randint(0, 1), random.randint(0, 1)]
-
-        self.a = entradas[0]
-        self.b = entradas[1]
+    def __init__(self, tamanyo=None, cordenadas=None):
 
         self.cordenadas = [40, 90, 75, 210, 240, 330, 475, 576]
         self.tamanyo = (58, 120)
         self.salida = None
+        self.entradas = None
+
+    def comp(self, inputs):
+        pass
 
